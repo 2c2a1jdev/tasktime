fb9b5eb0d2ac9305f3737add0135c7cd6df0ede6
Menueasy <menueasy@gmail.com>
2021-10-09 11:59:12 -0300

    nova função calculo tempo até conclusão

--- js/index.js
+++ js/index.js
@@ -25,11 +25,13 @@ window.onload = function renderOnScreen() {
             `
         }
 
+        const span = formatDiffDate(t.diff)
+
         if (t.isCompleted === true) {
             dataToShowIfComplete += `
             <div class="content">
                 <p>${t.taskValue}</p>
-                <p class="span">50 minutos</p>
+                <p class="span">${span}</p>
             </div>
             `
         }
@@ -59,11 +61,17 @@ function getArrOnLocalStorage() {
 function createNewTask(task) {
     let uid = Math.random().toString(12).slice(-5)
     task.toString()
+    let start = Date.now()
+    var end
+    var diff
     
     let tasks = {
         "ID": uid,
         "taskValue": task,
-        "isCompleted": false
+        "isCompleted": false,
+        "dateStart": start,
+        "dateEnd": end,
+        "diff": diff
     }
 
     return tasks
@@ -85,11 +93,15 @@ function saveOnLocalStorage() {
 }
 
 // Altera a propriedade isComplete de uma task de false para true
-function modifyStatusTask(valueIndex) {
+function modifyObjTask(valueIndex) {
+    end = Date.now()
     for(i=0; i < tasksArr.length; i++) {
         if (tasksArr[i].ID === valueIndex) {
             const y = tasksArr[i]
             y.isCompleted = true
+            y.dateEnd = end
+            diffDate = calcDiff(y.dateEnd, y.dateStart)
+            y.diff = diffDate
             localStorage.clear()
             const text = JSON.stringify(tasksArr)
             localStorage.setItem("tasksArr", text)
@@ -102,9 +114,8 @@ function modifyStatusTask(valueIndex) {
 function completeTask() {
     getArrOnLocalStorage()
     const taskToComplete = event.srcElement.value
-    modifyStatusTask(taskToComplete)
+    modifyObjTask(taskToComplete)
 }
-
 
 // Remove a task do localstorage
 function deleteTaskOnLocalStorage(valueIndexToDelete) {
@@ -118,10 +129,34 @@ function deleteTaskOnLocalStorage(valueIndexToDelete) {
     }
 }
 
-
 // Evento do user que exclui a task 
 function deleteTask() {
     getArrOnLocalStorage()
     const taskToDelete = event.srcElement.id
     deleteTaskOnLocalStorage(taskToDelete)
+}
+
+// Metodo que calcula a difereça em milisegundos entre o tempo de inserção e conclusão
+function calcDiff(start, end) {
+    let diffBetweenDates = Math.abs(end - start)
+    return diffBetweenDates
+}
+
+
+// Metodo que valida e formata a diferença de tempo e retona o texto ao user
+function formatDiffDate(diffInMilliseconds) {
+
+    let inSeconds, day, hours, minutes, seconds, span
+
+    inSeconds = diffInMilliseconds / 1000
+    day = parseInt(inSeconds / 86400)
+    inSeconds = inSeconds % 86400
+    hours = parseInt(inSeconds / 3600)
+    inSeconds = inSeconds % 3600 
+    minutes = parseInt(inSeconds / 60)
+    seconds = parseInt(inSeconds % 60)
+
+    span = `dias: ${day} / horas: ${hours} / minutos: ${minutes} / segundos: ${seconds}`
+    return span 
+
 }
